ca54b379cd6173790b5a9f004bf9c354c7d32ec8
milankap <milankap.vic@gmail.com>
2021-08-02 23:49:30 +0200

    fixed previous error, deleted middleware from production build

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const ShopPage = ({ match }) => (
     <div className="shop-page">
@@ -14,8 +12,4 @@ const ShopPage = ({ match }) => (
     </div>
 );
 
-const mapStateToProps = (state, ownProps) => ({
-    collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
